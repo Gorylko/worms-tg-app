6f1b56473a9707cb4333e086e794f8c26d343ece
Kiryl Shamiakou <Kiryl_Shamiakou@epam.com>
2024-04-22 11:46:34 +0200

    Initial bot api

---
+++ src/Worms.API/Worms.API/Controllers/BotController.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+
+namespace Worms.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BotController : ControllerBase
+{
+    [HttpPost("update")]
+    public async Task<IActionResult> ExecuteAsync(JsonDocument updateData)
+    {
+        var chatId = updateData.RootElement.GetProperty("message").GetProperty("chat").GetProperty("id").GetInt64();
+        var client = new TelegramBotClient("token");
+        await client.SendTextMessageAsync(chatId, "message");
+
+        return Ok();
+    }
+}
